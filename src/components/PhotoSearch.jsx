91080c146c7b92a8cc88c3e6e045a36ef6c87612
bisheskafle <goodkafle@gmail.com>
2022-04-26 11:39:23 +0545

    Loading

--- src/components/PhotoSearch.jsx
+++ src/components/PhotoSearch.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const PhotoSearch = () => {
   const [query, setQuery] = useState("");
   const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const changePhoto = () => {
+    setLoading(true);
     axios
       .get(
         `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=QjF9N1OKB6zi9jUrZoRPrOyEqW1QWNggpwtILlzMxS4`
       )
       .then((response) => {
         setPhotos(response.data.results);
+        setLoading(false);
       });
   };
+
+  if (loading) return <Skeleton count={70} />;
 
   return (
     <div>
@@ -36,7 +43,7 @@ const PhotoSearch = () => {
           {photos.map((photo) => {
             return (
               <div className="column">
-                <img src={photo.urls.small} />
+                <img src={photo.urls.small} alt={`${query}`} />
               </div>
             );
           })}
